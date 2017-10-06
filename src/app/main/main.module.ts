4c299af96c38721a2f9a3853c6ebe2f03fa86493
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-10-06 11:23:45 +0200

    Updating modules definitions

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
-import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
