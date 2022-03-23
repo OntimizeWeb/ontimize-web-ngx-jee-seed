02c7326343d113f808dc828c1c05153999d03e2a
Daniel Grana <daniel.grana@imatia.com>
2022-03-23 13:19:48 +0100

    Fix domchange.js

--- src/assets/js/domchange.js
+++ src/assets/js/domchange.js
@@ -1,5 +1,5 @@
 (function (window) {
-    var lastDate = +new Date();
+    var last = +new Date();
     var delay = 100; // default delay
 
     // Manage event queue
@@ -11,7 +11,7 @@
             for (var i = 0; i < stack.length; i++) {
                 stack[i]();
             }
-            lastDate = now;
+            last = now;
         }
     }
 
