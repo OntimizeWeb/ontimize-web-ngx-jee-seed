0018c31b895206c7f5b343e5fc6ab983c521f8cb
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2022-11-03 08:48:08 +0100

    Update versions ontimize  (#53)
    * Updated ontimize-web-ngx versions
    
    * Updated app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -29,5 +29,9 @@ export const CONFIG: Config = {
 
   appMenuConfiguration: MENU_CONFIG,
 
-  applicationLocales: ['es', 'en']
+  applicationLocales: ['es', 'en'],
+
+  exportConfiguration: {
+    path: '/export'
+  }
 };
